124be2c70d489811ef960f30a4d5985d5cf30b2a
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-02-25 11:59:26 -0800

    Add files via upload

---
+++ 2.7 Lab Ch 2: Basic output with variables.py
@@ -0,0 +1,8 @@
+userNum = int(input('Enter an integer:\n'))
+print("You entered:", userNum)
+print(userNum, 'squared is', userNum**2)
+print('And', userNum, 'cubed is', userNum**3, '!!')
+
+userNum2= int(input('Enter another integer:\n'))
+print(userNum, '+' ,userNum2, 'is', userNum + userNum2)
+print(userNum, '*' , userNum2, 'is', userNum*userNum2)
