169ff4971ea99dec8de804f2a17f671966ab2416
ryanhoang15 <61032642+ryanhoang15@users.noreply.github.com>
2020-03-11 11:43:48 -0700

    Add files via upload

---
+++ 6.20.py
@@ -0,0 +1,48 @@
+numbers = input("Enter a sequence of comma-separated numbers:\n ")
+if numbers == "" \
+              "":
+    exit()
+list1 = []
+numbers = numbers.split(",")
+
+for i in numbers:
+    if int(i) % 2 != 0:
+        str(i)
+        list1.append(i)
+        x = ",".join(list1)
+print(x)
+
+
+
+
+
+
+
+    # w=0
+    # if w < len
+    #     x.split()
+    #     print(x,end="")
+        # print(len(list1),end="")
+    #     a = num + ", "
+    #     a.split()
+    #     print(a,end="")
+    #  for w in range(len(a))
+    # if int(a[num]) != ",":
+    #     break
+    # print(int(a[num]))
+        # print(a,end="")
+# print(len(a),end="")
+
+
+# print(len(a),end="")
+
+    # for i in range(len(a) - 1, 0, -1):
+    #     if a[i] != ",":
+    #         print(a[:i + 1], end="")
+    #         break
+
+
+    # a=0
+    # if a < len(list1):
+    #     print(int(list1[x]) + (", "))
+    #     a = a + 1
